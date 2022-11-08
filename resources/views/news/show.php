3632a231287ffc34e78513b61a96adc8daff13b5
Ackap <ackapga@gmail.com>
2022-11-09 01:16:04 +0600

    Роутинг. Использование контроллеров

---
+++ resources/views/news/show.php
@@ -0,0 +1,6 @@
+<div style="border: 1px solid green">
+    <h2><?= $news['title'] ?></h2>
+    <p><?= $news['author'] ?> - <?= $news['create_at']->format('d-m-Y H:i') ?></p>
+    <p><?= $news['description'] ?></p>
+    <p><?= $news['status'] ?></p>
+</div>
