f6b91897e83fdcad85734bcafc03616debf013f1
Ackap <ackapga@gmail.com>
2022-11-27 22:16:57 +0600

    Валидация и вывод Сообщения__()

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\News\CreateRequest;
+use App\Http\Requests\News\EditRequest;
 use App\Models\Category;
 use App\Models\News;
-use App\Queries\CategoriesQueryBuilder;
 use App\Queries\NewsQueryBuilder;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
@@ -38,34 +38,24 @@ class NewsController extends Controller
     }
 
     /**
-     * @param Request $request
+     * @param CreateRequest $request
      * @param NewsQueryBuilder $builder
      * @return RedirectResponse
      */
     public function store(
-        Request $request,
+        CreateRequest    $request,
         NewsQueryBuilder $builder
-    ): RedirectResponse {
-        $request->validate([
-            'title' => ['required', 'string', 'min:3', 'max:255']
-        ]);
-
+    ): RedirectResponse
+    {
         $news = $builder->create(
-            $request->only([
-                'category_id',
-                'title',
-                'author',
-                'status',
-                'image',
-                'description',
-            ])
+            $request->validated()
         );
 
         if ($news) {
             return redirect()->route('admin.news.index')
-                ->with('success', 'Новость успешно добавленная!');
+                ->with('success', __('messages.admin.news.store.success'));
         }
-        return back()->with('error', 'Не удалось добавить новость!');
+        return back()->with('error', __('messages.admin.news.store.error'));
     }
 
     /**
@@ -82,28 +72,22 @@ class NewsController extends Controller
     }
 
     /**
-     * @param Request $request
+     * @param EditRequest $request
      * @param News $news
      * @param NewsQueryBuilder $builder
      * @return RedirectResponse
      */
     public function update(
-        Request          $request,
+        EditRequest      $request,
         News             $news,
         NewsQueryBuilder $builder
-    ): RedirectResponse {
-        if ($builder->update($news, $request->only([
-            'category_id',
-            'title',
-            'author',
-            'status',
-            'image',
-            'description',
-        ]))) {
+    ): RedirectResponse
+    {
+        if ($builder->update($news, $request->validated())) {
             return redirect()->route('admin.news.index')
-                ->with('success', 'Новость успешно обновлена!');
+                ->with('success', __('messages.admin.news.update.success'));
         }
-        return back()->with('error', 'Не удалось обновить новость!');
+        return back()->with('error', __('messages.admin.news.update.error'));
     }
 
     /**
