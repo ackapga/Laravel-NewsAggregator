ab5ed78b9767963f82fece8ca89761e27edf774a
Ackap <ackapga@gmail.com>
2022-11-30 00:48:33 +0600

    Навел Красоту

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -4,16 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 
 class IndexController extends Controller
 {
     /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Application|Factory|View
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): View|Factory|Application
     {
         return view('admin.index');
     }
--- app/Http/Controllers/News/NewsController.php
+++ app/Http/Controllers/News/NewsController.php
@@ -5,17 +5,19 @@ namespace App\Http\Controllers\News;
 use App\Http\Controllers\Controller;
 use App\Models\News;
 use App\Queries\NewsQueryBuilder;
-use function view;
+use Illuminate\Contracts\View\View;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\Foundation\Application;
 
 class NewsController extends Controller
 {
-    public function index(NewsQueryBuilder $builder)
+    public function index(NewsQueryBuilder $builder): Factory|View|Application
     {
         $news = $builder->getNewsForList();
        return view('news.index', ['newsList' => $news]);
     }
 
-    public function show(int $id)
+    public function show(int $id): Factory|View|Application
     {
         $news = app(News::class)->getNewsById($id);
         return view('news.show', ['news' => $news]);
