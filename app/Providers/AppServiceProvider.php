f5ee0350ce39a7742256a8707ff65a750e57aab8
Ackap <ackapga@gmail.com>
2022-12-17 15:25:04 +0600

    Изменил Стиль и Содержание Писма на почту)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,11 @@ use App\Services\Contracts\Parser;
 use App\Services\Contracts\Social;
 use App\Services\ParsesService;
 use App\Services\SocialService;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -33,5 +37,22 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFive();
+
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject(Lang::get('Подтвердите адрес электронной почты'))
+                ->line(Lang::get('Нажмите кнопку ниже, чтобы подтвердить свой адрес электронной почты.'))
+                ->action(Lang::get('Подтвердите адрес электронной почты'), $url)
+                ->line(Lang::get('Если вы не создавали учетную запись, никаких дальнейших действий не требуется.'));
+        });
+
+        ResetPassword::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage())
+                ->subject(Lang::get('Уведомление о сбросе пароля'))
+                ->line(Lang::get('Вы получили это письмо, потому что мы получили запрос на сброс пароля для вашей учетной записи.'))
+                ->action(Lang::get('Сброс пароля'), $url)
+                ->line(Lang::get('Срок действия этой ссылки для сброса пароля истекает через :count минут.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+                ->line(Lang::get('Если вы не запрашивали сброс пароля, никаких дальнейших действий не требуется.'));
+        });
     }
 }
